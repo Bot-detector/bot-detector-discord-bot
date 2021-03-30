9e64f634f201c6a44e4d2f8d415cec09959b316e
Matthew Thornton <thorntonmatthewd@gmail.com>
2021-03-30 03:11:12 -0400

    Updated startup script

--- startup.sh
+++ startup.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-if [ -e pid.txt ]
-then
-    kill -9 `cat pid.txt`
-    rm pid.txt
-else
-    echo "no file bro"
+pkill -f python3
 
 nohup python3 main.py  > bot.log 2>&1 &
-echo $! > pid.txt
 
-fi
