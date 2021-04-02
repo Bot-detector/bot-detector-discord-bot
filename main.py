e7a33b2cebbbae67447790cbd2dfc18f236eafcc
Ferrariic <thearcticfalcon@gmail.com>
2021-04-01 21:34:51 -0400

    Updated !rules, !issues

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ async def on_message(message):
         await message.channel.send('Chop chop!')
         
     if message.content.startswith('!rules') or message.content.startswith('!Rules'):
-        await message.channel.send('#rules-and-information')
+        await message.channel.send('<#${825137784112807946}>')
         
     if message.content.startswith('!list') or message.content.startswith('!List'):
         await message.channel.send('PENDING - Will be added soon :) Stay Tuned')
@@ -47,7 +47,7 @@ async def on_message(message):
         await message.channel.send('https://discord.com/invite/JCAGpcjbfP')
 
     if message.content.startswith('!issues') or message.content.startswith('!Issues'):
-        await message.channel.send('#issues')
+        await message.channel.send('<#${822851862016950282}>')
 
     if message.content.startswith('!stats') or message.content.startswith('!STATS'):
         playersTrackedResponse = req.get("https://www.osrsbotdetector.com/api/site/dashboard/gettotaltrackedplayers")
