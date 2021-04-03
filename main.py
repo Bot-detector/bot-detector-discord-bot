07f3375ad49034bd9f3113f3520a490fd2924777
Ferrariic <thearcticfalcon@gmail.com>
2021-04-03 18:41:36 -0400

    removed !rsn

--- main.py
+++ main.py
@@ -149,14 +149,6 @@ async def on_message(message):
         await message.channel.send(msg)
         
     # player stats
-    
-    if message.content.startswith('!rsn') or message.content.startswith('!RSN'):
-        discordRSN = message.content[5:17]
-        msg = "```diff" + "\n" \
-                 + "+ RSN set to: " + str(discordRSN) + "\n" \
-                 + "Linked to Discord: " str(message.author.id) + "\n" \
-                 + "```\n"
-        await message.channel.send(msg)
 
     if message.content.startswith('!kc') or message.content.startswith('!KC'):
         playerName = message.content[4:16]
