5dff697a4e12e4fd38989d19232dc69440b1575d
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-04-11 18:56:19 -0400

    Moved predict command back into the channel restriction check.

--- main.py
+++ main.py
@@ -204,105 +204,105 @@ async def on_message(message):
       
       # list dm process
 
-      if message.content.startswith('!list') or message.content.startswith('!List'):
-          msg = "Please send a link to a Pastebin URL containing your name list." + "\n" \
-          + "Example: !submit https://pastebin.com/iw8MmUzg" + "\n" \
-          + "___________" + "\n" \
-          + "Acceptable Formatting:" + "\n" \
-          + "Player 1" + "\n" \
-          + "Player 2" + "\n" \
-          + "Player 3" + "\n" \
-          + "Player 4" + "\n" \
-          + "Player 5" + "\n" \
-          + "___________" + "\n" \
-          + "Pastebin Settings:" + "\n" \
-          + "Syntax Highlighting: None" + "\n" \
-          + "Paste Expiration: 1 Day" + "\n" \
-          + "Paste Exposure: Public" + "\n" \
-          + "Folder: No Folder Selected" + "\n" \
-          + "Password: {leave blank - no password needed}" + "\n" \
-          + "Paste Name / Title: {Include your Label Here}" + "\n" 
-          await message.author.send(msg)
+        if message.content.startswith('!list') or message.content.startswith('!List'):
+            msg = "Please send a link to a Pastebin URL containing your name list." + "\n" \
+            + "Example: !submit https://pastebin.com/iw8MmUzg" + "\n" \
+            + "___________" + "\n" \
+            + "Acceptable Formatting:" + "\n" \
+            + "Player 1" + "\n" \
+            + "Player 2" + "\n" \
+            + "Player 3" + "\n" \
+            + "Player 4" + "\n" \
+            + "Player 5" + "\n" \
+            + "___________" + "\n" \
+            + "Pastebin Settings:" + "\n" \
+            + "Syntax Highlighting: None" + "\n" \
+            + "Paste Expiration: 1 Day" + "\n" \
+            + "Paste Exposure: Public" + "\n" \
+            + "Folder: No Folder Selected" + "\n" \
+            + "Password: {leave blank - no password needed}" + "\n" \
+            + "Paste Name / Title: {Include your Label Here}" + "\n" 
+            await message.author.send(msg)
 
-      if message.content.startswith('!submit') or message.content.startswith('!Submit'):
-          paste_url = message.content[8:100]
+        if message.content.startswith('!submit') or message.content.startswith('!Submit'):
+            paste_url = message.content[8:100]
 
-          newlines, label = get_paste_names(paste_url)
-          label_insert(label)
-          name_insert(newlines)
-          player_label_join(label, newlines)
+            newlines, label = get_paste_names(paste_url)
+            label_insert(label)
+            name_insert(newlines)
+            player_label_join(label, newlines)
 
-          msg = "Paste Information" + "\n" \
-          + "_____________________" + "\n" \
-          + "Number of Names: " + str(len(newlines)) + "\n" \
-          + "Label: " + str(label) + "\n" \
-          + "Samples: " + str(newlines[0:10]) + "\n" \
-          + "Link: " + str(paste_url) + "\n"
+            msg = "Paste Information" + "\n" \
+                + "_____________________" + "\n" \
+                + "Number of Names: " + str(len(newlines)) + "\n" \
+                + "Label: " + str(label) + "\n" \
+                + "Samples: " + str(newlines[0:10]) + "\n" \
+                + "Link: " + str(paste_url) + "\n"
 
-          user = client.get_user(int(os.getenv('SUBMIT_RECIPIENT')))
+            user = client.get_user(int(os.getenv('SUBMIT_RECIPIENT')))
 
-          await user.send(msg)
+            await user.send(msg)
 
       # links
 
-      if message.content.startswith('!website') or message.content.startswith('!Website'):
-          await message.channel.send('https://www.osrsbotdetector.com/#/')
+        if message.content.startswith('!website') or message.content.startswith('!Website'):
+            await message.channel.send('https://www.osrsbotdetector.com/#/')
 
-      if message.content.startswith('!patreon') or message.content.startswith('!Patreon'):
-          await message.channel.send('https://www.patreon.com/bot_detector') 
+        if message.content.startswith('!patreon') or message.content.startswith('!Patreon'):
+            await message.channel.send('https://www.patreon.com/bot_detector') 
 
-      if message.content.startswith('!github core') or message.content.startswith('!github core'):
-              await message.channel.send('https://github.com/Ferrariic/Bot-Detector-Core-Files') 
+        if message.content.startswith('!github core') or message.content.startswith('!github core'):
+            await message.channel.send('https://github.com/Ferrariic/Bot-Detector-Core-Files') 
 
-      if message.content.startswith('!github plugin') or message.content.startswith('!github plugin'):
-              await message.channel.send('https://github.com/Ferrariic/bot-detector') 
+        if message.content.startswith('!github plugin') or message.content.startswith('!github plugin'):
+            await message.channel.send('https://github.com/Ferrariic/bot-detector') 
 
-      if message.content.startswith('!invite') or message.content.startswith('!Invite'):
-          await message.channel.send('https://discord.com/invite/JCAGpcjbfP')
+        if message.content.startswith('!invite') or message.content.startswith('!Invite'):
+            await message.channel.send('https://discord.com/invite/JCAGpcjbfP')
 
       # plugin and database stats
 
-      if message.content.startswith('!stats') or message.content.startswith('!STATS'):
-          playersTrackedResponse = req.get("https://www.osrsbotdetector.com/api/site/dashboard/gettotaltrackedplayers")
-          otherStatsResponse = req.get("https://www.osrsbotdetector.com/api/site/dashboard/getreportsstats")
-          activeInstallsReponse = req.get("https://api.runelite.net/runelite-1.7.4/pluginhub")
+        if message.content.startswith('!stats') or message.content.startswith('!STATS'):
+            playersTrackedResponse = req.get("https://www.osrsbotdetector.com/api/site/dashboard/gettotaltrackedplayers")
+            otherStatsResponse = req.get("https://www.osrsbotdetector.com/api/site/dashboard/getreportsstats")
+            activeInstallsReponse = req.get("https://api.runelite.net/runelite-1.7.4/pluginhub")
 
-          playersJSON = playersTrackedResponse.json()
-          otherStatsJSON= otherStatsResponse.json()
-          activeInstallsJSON = activeInstallsReponse.json()
+            playersJSON = playersTrackedResponse.json()
+            otherStatsJSON= otherStatsResponse.json()
+            activeInstallsJSON = activeInstallsReponse.json()
 
-          playersTracked = playersJSON['players'][0]
-          totalBans = otherStatsJSON['bans']
-          totalReports = otherStatsJSON['total_reports']
-          activeInstalls = activeInstallsJSON['bot-detector']
+            playersTracked = playersJSON['players'][0]
+            totalBans = otherStatsJSON['bans']
+            totalReports = otherStatsJSON['total_reports']
+            activeInstalls = activeInstallsJSON['bot-detector']
 
-          msg = "```Project Stats:\n" \
-                  + "Players Analyzed: " + str(playersTracked) + "\n"\
-                  + "Reports Sent to Jagex: " + str(totalReports) + "\n"\
-                  + "Resultant Bans: " + str(totalBans) + "\n"\
-                  + "Active Installs: " + str(activeInstalls) \
-                  + "```"
+            msg = "```Project Stats:\n" \
+                + "Players Analyzed: " + str(playersTracked) + "\n"\
+                + "Reports Sent to Jagex: " + str(totalReports) + "\n"\
+                + "Resultant Bans: " + str(totalBans) + "\n"\
+                + "Active Installs: " + str(activeInstalls) \
+                + "```"
 
-          await message.channel.send(msg)
+            await message.channel.send(msg)
 
       # player stats
 
-      if message.content.startswith('!kc') or message.content.startswith('!KC'):
-          playerName = message.content[4:16]
+        if message.content.startswith('!kc') or message.content.startswith('!KC'):
+            playerName = message.content[4:16]
 
-          resp = req.get("https://www.osrsbotdetector.com/api/stats/contributions/" + playerName)
-          respJSON = resp.json()
+            resp = req.get("https://www.osrsbotdetector.com/api/stats/contributions/" + playerName)
+            respJSON = resp.json()
 
-          reports = respJSON['reports']
-          bans = respJSON['bans']
-          possible_bans = respJSON['possible_bans']
+            reports = respJSON['reports']
+            bans = respJSON['bans']
+            possible_bans = respJSON['possible_bans']
 
-          msg = "```" + playerName + "'s Stats: \n" \
-                   + "Reports Submitted: " + str(reports) + "\n" \
-                   + "Probable/Pending Bans: " + str(possible_bans) + "\n" \
-                   + "Confirmed Bans: " + str(bans) + "```\n"
+            msg = "```" + playerName + "'s Stats: \n" \
+                + "Reports Submitted: " + str(reports) + "\n" \
+                + "Probable/Pending Bans: " + str(possible_bans) + "\n" \
+                + "Confirmed Bans: " + str(bans) + "```\n"
 
-          await message.channel.send(msg)
+            await message.channel.send(msg)
 
       #predict method
 
