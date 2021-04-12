f8f3967604769171743526e9526aca93bf05cc15
Ferrariic <thearcticfalcon@gmail.com>
2021-04-12 17:52:31 -0400

    added !verify

--- main.py
+++ main.py
@@ -199,12 +199,14 @@ def verification_check(player_id):
     data = mycursor.fetchmany(size=1)
     if len(data)>0:
         check = True
+        verified = data[0][-1]
+        owner_id = data[0][1]
     else:
         check = False
 
     mycursor.close()
     mydb.close()
-    return check
+    return check, verified, owner_id
 
 def verificationInsert(discord_id, player_id, code):
     mydb = mysql.connector.connect(**config_submissions)
@@ -287,47 +289,90 @@ async def on_message(message):
     if message.channel.id == 825189024074563614 or message.channel.type == 'dm':
       
         if message.content.startswith('!link') or message.content.startswith('!Link'):
-            playerName = message.content[6:18]
-            code = id_generator()
-            discord_id = message.author.id
+          playerName = message.content[6:18]
+          code = id_generator()
+          discord_id = message.author.id
+
+          msgPassed = "```diff" + "\n" \
+          + "====== INFO ======\n" \
+          + "Request to link: " + str(playerName) + "\n" \
+          + "Your discord ID is: " + str(discord_id) + "\n" \
+          + "====== SETUP ======\n" \
+          + "+ Please submit the access code below in the form of a DM in-game to 'Ferrariic' or in the clan chat 'Bot Detector'." + "\n" \
+          + "+ Access Code: " + str(code)+ "\n" \
+          + "+ A message will be sent to you on Discord when your account has been successfully paired." + "\n" \
+          + "====== NOTICE ======\n" \
+          + "- If this RSN was submitted in error, please type !link <Your Correct RSN>" + "\n" \
+          + "- This code will expire in 24 hours." + "\n" \
+          + "- Do not share this code with anyone." + "\n" \
+          + "```"
+
+          msgInUse = "```diff" + "\n" \
+          + "- RSN is currently in use. Please contact an Administrator" + "\n" \
+          + "```"
+
+          msgInstallPlugin = "```diff" + "\n" \
+          + "- This user has not installed the Bot Detector plugin, or this user does not exist." + "\n" \
+          + "- Please install the plugin or re-enter your !link <RSN> command." + "\n" \
+          + "```"
+
+          msgVerified = "```diff" + "\n" \
+          + "+ Player: " + str(playerName) + "\n" \
+          + "====== Verification Information ======\n" \
+          + "+ Player is: Verified." + "\n" \
+          + "+ Discord Verified Under: " + "<@" + str(owner_id) + ">" + "\n" \
+          + "```"
+
+          msgUnverified = "```diff" + "\n" \
+          + "+ Player: " + str(playerName) + "\n" \
+          + "====== Verification Information ======\n" \
+          + "- Player is: Unverified." + "\n" \
+          + "```"
+
+          player_id, exists = verificationPull(playerName)
+          if exists:
+              check, verified, owner_id = verification_check(player_id)
+              if verified:
+                  msg = msgVerified
+              else:
+                  if check:
+                      msg = msgInUse
+                  else:
+                      verificationInsert(discord_id, player_id, code)
+                      msg = msgPassed
+          else:
+              msg = msgInstallPlugin
+
+          await message.author.send(msg)
+
+        if message.content.startswith('!verify') or message.content.startswith('!Verify'):
+            playerName = message.content[8:20]
             
-            msgPassed = "```diff" + "\n" \
-                + "====== INFO ======\n" \
-                + "Request to link: " + str(playerName) + "\n" \
-                + "Your discord ID is: " + str(discord_id) + "\n" \
-                + "====== SETUP ======\n" \
-                + "+ Please submit the access code below in the form of a DM in-game to 'Ferrariic' or in the clan chat 'Bot Detector'." + "\n" \
-                + "+ Access Code: " + str(code)+ "\n" \
-                + "+ A message will be sent to you on Discord when your account has been successfully paired." + "\n" \
-                + "====== NOTICE ======\n" \
-                + "- If this RSN was submitted in error, please type !link <Your Correct RSN>" + "\n" \
-                + "- This code will expire in 24 hours." + "\n" \
-                + "- Do not share this code with anyone." + "\n" \
-                + "```"
-            
-            msgInUse = "```diff" + "\n" \
-              + "- RSN is currently in use. Please contact an Administrator" + "\n" \
-              + "```"
-            
-            msgInstallPlugin = "```diff" + "\n" \
-              + "- This user has not installed the Bot Detector plugin, or this user does not exist." + "\n" \
-              + "- Please install the plugin or re-enter your !link <RSN> command." + "\n" \
-              + "```"
+            msgVerified = "```diff" + "\n" \
+            + "+ Player: " + str(playerName) + "\n" \
+            + "====== Verification Information ======\n" \
+            + "+ Player is: Verified." + "\n" \
+            + "+ Discord Verified Under: " + "<@" + str(owner_id) + ">" + "\n" \
+            + "```"
+
+            msgUnverified = "```diff" + "\n" \
+            + "+ Player: " + str(playerName) + "\n" \
+            + "====== Verification Information ======\n" \
+            + "- Player is: Unverified." + "\n" \
+            + "```"
             
             player_id, exists = verificationPull(playerName)
             if exists:
-                check = verification_check(player_id)
-                if check:
-                    msg = msgInUse
+                check, verified, owner_id = verification_check(player_id)
+                if verified:
+                    msg = msgVerified
                 else:
-                    verificationInsert(discord_id, player_id, code)
-                    msg = msgPassed
+                    msg = msgUnverified
             else:
-                msg = msgInstallPlugin
-    
-            await message.author.send(msg)
+                msg = msgUnverified
             
-
+            await message.channel.send(msg)
+     
         if message.content.startswith('!list') or message.content.startswith('!List'):
             msg = "Please send a link to a Pastebin URL containing your name list." + "\n" \
             + "Example: !submit https://pastebin.com/iw8MmUzg" + "\n" \
