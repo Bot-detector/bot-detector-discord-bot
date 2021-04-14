8df0f8ac28ae42e8d927807b50eb60be077fb863
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-04-14 15:09:20 -0400

    Added name from prediction in request.

--- reaction_commands.py
+++ reaction_commands.py
@@ -17,8 +17,9 @@ async def add_prediction_feedback(payload, message):
 
     prediction = {
         "discord_id":   payload.user_id,
-        "prediction":   prediction_data[0],
-        "confidence":   prediction_data[1],
+        "name":         prediction_data[0],
+        "prediction":   prediction_data[1],
+        "confidence":   prediction_data[2],
         "vote":         vote
     }
 
@@ -30,15 +31,18 @@ async def add_prediction_feedback(payload, message):
 
 
 def extract_prediction(message):
+    name_substring = "+ Name: "
     prediction_substring = "+ Prediction: "
     confidence_substring = "+ Confidence: "
 
     message_lines = message.content.splitlines()
 
+    name_line = [i for i in message_lines if name_substring in i]
     prediction_line = [i for i in message_lines if prediction_substring in i]
     confidence_line = [i for i in message_lines if confidence_substring in i]
 
+    name = name_line[0].split(name_substring)[1]]
     prediction = prediction_line[0].split(prediction_substring)[1]
     confidence = confidence_line[0].split(confidence_substring)[1]
 
-    return prediction, float(confidence)
+    return name, prediction, float(confidence)
