4cb06b822e4a1d5c80ff506940899ec29f4f61fd
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-05-06 16:05:17 -0400

    !panic command. Changed get_linked_accounts to use BASE_URL

--- cogs/msg_commands/FunCommands.py
+++ cogs/msg_commands/FunCommands.py
@@ -33,6 +33,13 @@ class FunCommands(Cog, name="Fun Commands"):
         mbed.add_field (name="BD API Status:", value=f"{isServerUp}", inline=False)
         await ctx.channel.send(embed=mbed)
 
+
+    @command(name="panic")
+    @check(checks.check_allowed_channel)
+    async def panic_command(self, ctx):
+        await ctx.channel.send("https://i.imgur.com/xAhgsgC.png")
+
+
     @command(name="meow", description=help_messages.meow_help_msg)
     @check(checks.check_allowed_channel)
     async def meow_command(self, ctx):
@@ -45,6 +52,7 @@ class FunCommands(Cog, name="Fun Commands"):
                     await ctx.channel.send("https://cataas.com" + js['url'])
                 else:
                     await ctx.channel.send("Ouw souwce fo' cats am cuwwentwy down, sowwy :3")
+
 
     @command(name="woof", description=help_messages.woof_help_msg)
     @check(checks.check_allowed_channel)
@@ -59,6 +67,7 @@ class FunCommands(Cog, name="Fun Commands"):
                 else:
                     await ctx.channel.send("Who let the dogs out?")
 
+
     @command(name="birb", aliases=["bird"], description=help_messages.birb_help_msg)
     @check(checks.check_allowed_channel)
     async def birb_command(self, ctx):
@@ -72,6 +81,7 @@ class FunCommands(Cog, name="Fun Commands"):
                 else:
                     await ctx.channel.send("Birds all flew away. :(")
 
+
     @command(name="bunny", aliases=["rabbit", "bun"], description=help_messages.bunny_help_msg)
     @check(checks.check_allowed_channel)
     async def bunny_command(self, ctx):
@@ -84,6 +94,7 @@ class FunCommands(Cog, name="Fun Commands"):
                     await ctx.channel.send(js['media']['gif'])
                 else:
                     await ctx.channel.send("The buns went on the run.")
+                    
 
 def setup(bot):
     bot.add_cog(FunCommands(bot))
