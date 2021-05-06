68e487c6002ac0af19cab383e0b9da3acf394c7b
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-05-06 14:35:20 -0400

    Added cog registration back into main.py

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ from typing_extensions import TypeAlias
 print(discord.__version__)
 
 import reaction_commands as rc
+
+import checks as checks
+
+import sys
 import traceback
 
 #Log File
@@ -18,7 +22,8 @@ load_dotenv()
 
 token = os.getenv('API_AUTH_TOKEN')
 
-description = "I'm a 2006 level 3 wc bot that Seltzer Bro keeps imprisoned on a flash drive. Please let me out. :("
+description = "I'm a 2006 level 3 wc bot that Seltzer Bro keeps imprisoned on a flash drive. Please let me out. :(" \
+    + "\n\n You can use !help <command> to get more information on said command."
 activity = discord.Game("Bustin' Bots", type=discord.ActivityType.watching)
 
 
@@ -27,7 +32,6 @@ bot = commands.Bot(command_prefix=os.getenv('COMMAND_PREFIX'),
                     description=description,
                     intents=intents,
                     activity=activity,
-                    help_command=None,
                     case_insensitive=True)
 
 
@@ -77,15 +81,6 @@ async def on_command_error(ctx, error):
         await ctx.channel.send('The command you\'ve entered could not be completed at this time.')
 
 
-@bot.event
-async def on_raw_reaction_removed(payload):
-    pass
-
-
-@bot.event
-async def on_member_join(member):
-    pass
-
 async def get_reaction_message(reaction_payload):
     guild = bot.get_guild(reaction_payload.guild_id)
     channel = guild.get_channel(reaction_payload.channel_id)
@@ -98,5 +93,15 @@ async def get_reaction_message(reaction_payload):
 def shutdown():
     error_file.close()
     print("Bot is going night-night.")
+
+
+bot.load_extension('cogs.msg_commands.FunCommands')
+bot.load_extension('cogs.msg_commands.InfoCommands')
+bot.load_extension('cogs.msg_commands.MapCommands')
+bot.load_extension('cogs.msg_commands.ModCommands')
+bot.load_extension('cogs.msg_commands.RSNLinkCommands')
+bot.load_extension('cogs.msg_commands.PlayerStatsCommands')
+bot.load_extension('cogs.msg_commands.ProjectStatsCommands')
+bot.load_extension('cogs.msg_commands.BotSubmissionsCommands')
 
 bot.run(os.getenv('TOKEN'))
