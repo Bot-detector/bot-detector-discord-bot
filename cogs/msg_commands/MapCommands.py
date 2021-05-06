19831d1fa65d9439637be7cd3626e74ea88d43f2
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-05-06 02:40:38 -0400

    !map fix as well

--- cogs/msg_commands/MapCommands.py
+++ cogs/msg_commands/MapCommands.py
@@ -143,6 +143,16 @@ class MapCommands(Cog, name='Map Commands'):
             dataRegion = await map_processing.getHeatmapRegion(regionName, token)
             dfDataRegion = pd.DataFrame(dataRegion)
             dfRegion = map_processing.displayDuplicates(dfDataRegion)
+
+            if len(dfRegion) == 0:
+                    mbed = discord.Embed (
+                        description = f"\"{regionName}\" does not correspond with any of our labeled regions." \
+                            + " It is possible that we just need to add it. Please let us know if so!",
+                        color = discord.Colour.dark_red()
+                    )
+
+                    await ctx.channel.send(embed=mbed)
+                    return
         
             if len(dfRegion) < 30:
                 regionTrueName, region_id = map_processing.Autofill(dfRegion, regionName)
