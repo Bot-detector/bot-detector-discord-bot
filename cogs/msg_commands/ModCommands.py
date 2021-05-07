0aee91e11106df14441ef34a100d1c02c278255e
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-05-06 23:35:36 -0400

    Preventing any other catastrophes, no matter how :wyvernpog: worthy they may be.

--- cogs/msg_commands/ModCommands.py
+++ cogs/msg_commands/ModCommands.py
@@ -19,66 +19,52 @@ class ModCommands(Cog, name='Moderator Commands'):
     async def warn_command(self, ctx):
         mbed = await warn_msg()
         await ctx.channel.send(embed=mbed)
-
-    @command(name="test", hidden=True)
-    async def test_command(self, ctx):
-        #mbed = await warn_msg()
-        print(dir(ctx.guild))
-        await ctx.guild.get_channel(825189024074563614).send("test")
+        
 
     @command(name="updateallroles", hidden=True)
     @has_permissions(manage_roles=True)
-    async def update_all_roles_command(self, ctx, spam):
+    async def update_all_roles_command(self, ctx, spamChoice=""):
 
-        if spam is "spam":
-            sendSpam = True
+        if 'spam' in spamChoice:
+            await ctx.channel.send(f"{ctx.author.mention} You are a monster. I won't do it!!")
         else:
-            sendSpam = False
+            pass
 
         listUsers = await discord_processing.get_discords_ids_with_links(token)
         print(listUsers)
 
         for user in listUsers:
-            member = await ctx.guild.fetch_member(user['Discord_id'])
-            print(member)
+            try:
+                member = await ctx.guild.fetch_member(user['Discord_id'])
+                print(member)
 
-            linkedAccounts = await discord_processing.get_linked_accounts(member.id, token)
-            print(linkedAccounts)
+                linkedAccounts = await discord_processing.get_linked_accounts(member.id, token)
+                print(linkedAccounts)
 
-            if(len(linkedAccounts) == 0):
-                #how the heck did we get here
-                pass
-            else:
-                for r in member.roles:
-                    if r.id == roles.special_roles["verified_rsn"]:
-                        #awesome, you're verified.
-                        break
-
-                else:
-                    verified_role = discord.utils.find(lambda r: r.id == roles.special_roles["verified_rsn"], member.guild.roles)
-                    await member.add_roles(verified_role)
-
-            current_role = discord.utils.find(lambda r: 'Bot Hunter' in r.name, member.roles)
-            new_role = await roles.get_bot_hunter_role(linkedAccounts, member)
-
-            if(new_role == False):
-                pass
-            else:
-                await roles.remove_old_roles(member)
-                await member.add_roles(new_role)
-
-                if new_role is not current_role and sendSpam:
-                    mbed = discord.Embed (
-                            description = f"{member.display_name}, you are now a {new_role}!",
-                            color = new_role.color
-                        )
-
-                    mbed.set_thumbnail(url="https://user-images.githubusercontent.com/45152844/116952387-8ac1fa80-ac58-11eb-8a31-5fe0fc6f5f88.gif")
-
-                    await ctx.guild.get_channel(825189024074563614).send(embed=mbed)
-
-                else:
+                if(len(linkedAccounts) == 0):
+                    #how the heck did we get here
                     pass
+                else:
+                    for r in member.roles:
+                        if r.id == roles.special_roles["verified_rsn"]:
+                            #awesome, you're verified.
+                            break
+
+                    else:
+                        verified_role = discord.utils.find(lambda r: r.id == roles.special_roles["verified_rsn"], member.guild.roles)
+                        await member.add_roles(verified_role)
+
+                current_role = discord.utils.find(lambda r: 'Bot Hunter' in r.name, member.roles)
+                new_role = await roles.get_bot_hunter_role(linkedAccounts, member)
+
+                if(new_role == False):
+                    pass
+                else:
+                    await roles.remove_old_roles(member)
+                    await member.add_roles(new_role)
+                    
+            except Exception as e:
+                    print(e)
         
         return
 
