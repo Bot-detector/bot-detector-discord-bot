adc88b35f3a4f768fa01f2e8f01dce918d256544
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-05-07 12:34:36 -0400

    Removed now unnecessary check in on_command_error for !help

--- main.py
+++ main.py
@@ -69,9 +69,7 @@ async def on_raw_reaction_add(payload):
 
 @bot.event
 async def on_command_error(ctx, error):
-    if ctx.invoked_with == "help":
-        return
-    elif isinstance(error, commands.CommandNotFound):
+    if isinstance(error, commands.CommandNotFound):
         return
     elif isinstance(error, discord.errors.Forbidden):
         return await ctx.send("I couldn't send this information to you via direct message. Are your DMs enabled?")
