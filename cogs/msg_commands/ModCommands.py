398b57ae0b665347fc3efabeaca785d73d94b5f9
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-05-08 21:05:44 -0400

    Made skull icon smaller on warn.

--- cogs/msg_commands/ModCommands.py
+++ cogs/msg_commands/ModCommands.py
@@ -106,7 +106,7 @@ async def warn_msg():
     mbed = discord.Embed(title=f"WARNING", color=0xff0000)
     mbed.add_field (name="= WARNING MESSAGE =", value="**Do not attempt to contact the Jmods or Admins in any channel regarding the status of your Runescape account: Doing so will result in an automatic permanent ban.**" + "\n" \
             + "**This is your only warning.**" + "\n", inline=False)
-    mbed.set_image(url="https://user-images.githubusercontent.com/5789682/117366156-59327480-ae8e-11eb-8b08-6cf815d8a36e.png")
+    mbed.set_thumbnail(url="https://user-images.githubusercontent.com/5789682/117366156-59327480-ae8e-11eb-8b08-6cf815d8a36e.png")
     return mbed
 
 
