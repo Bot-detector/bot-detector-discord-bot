e195f12779719565b5ba7f8b017b88e496eb73ca
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-08-19 13:28:28 -0400

    Display RSN for !kc as originally typed

--- cogs/msg_commands/PlayerStatsCommands.py
+++ cogs/msg_commands/PlayerStatsCommands.py
@@ -97,9 +97,9 @@ class PlayerStatsCommands(utils.CommonCog, name='Player Stats Commands'):
 
         elif utils.is_valid_rsn(player_name):
 
-            player_name = string_processing.to_jagex_name(player_name)
+            normalized_player_name = string_processing.to_jagex_name(player_name)
 
-            async with self.bot.session.get(f"https://www.osrsbotdetector.com/api/stats/contributions/{player_name}") as r:
+            async with self.bot.session.get(f"https://www.osrsbotdetector.com/api/stats/contributions/{normalized_player_name}") as r:
                 if r.status != 200:
                     return await ctx.reply(f"Couldn't grab the !kc for {player_name}")
 
