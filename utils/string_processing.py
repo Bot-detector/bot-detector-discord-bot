39a5b8049d894d7055078f4e0dd8e20fc5d90939
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-08-24 08:52:01 -0400

    Allow for special characters as the first character of RSNs

--- utils/string_processing.py
+++ utils/string_processing.py
@@ -9,7 +9,9 @@ def id_generator(size=4, chars=string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 def to_jagex_name(name: str) -> str:
-    return name.replace('_', ' ').replace('-', ' ').strip()
+    #Allow for special characters as the first character of RSNs
+    jagex_name = name[0:1] + name[1:].replace('_', ' ').replace('-', ' ').strip()
+    return jagex_name
 
 def plus_minus(var, compare):
     diff_control = '-'
