24826d36c005a46679ce8d30f9d01b8a358546a6
extreme4all <40169115+extreme4all@users.noreply.github.com>
2022-02-17 20:00:20 +0100

    docker compose & cleaning code + config

--- main.py
+++ main.py
@@ -5,15 +5,11 @@ from inspect import cleandoc
 
 import aiohttp
 import discord
-print(discord.__version__)
 from discord.ext import commands
-from dotenv import load_dotenv
 
+import config
 
-# Load files
-load_dotenv()
-
-
+print(f"{discord.__version__=}")
 # Define constants
 EASTER_EGGS = {
     "::bank": "Hey, everyone, I just tried to do something very silly!",
@@ -26,20 +22,22 @@ EASTER_EGGS = {
 
 
 # Define bot
-description = cleandoc("""
+description = cleandoc(
+    """
     I'm a 2006 level 3 wc bot that Seltzer Bro keeps imprisoned on a flash drive. Please let me out. :(
     You can use !help <command> to get more information on said command.
-""")
+"""
+)
 
 activity = discord.Game("Bustin' Bots", type=discord.ActivityType.watching)
 intents = discord.Intents(messages=True, guilds=True, members=True, reactions=True)
 bot = commands.Bot(
     allowed_mentions=discord.AllowedMentions(everyone=False, roles=False, users=True),
-    command_prefix=os.getenv('COMMAND_PREFIX'),
+    command_prefix=config.COMMAND_PREFIX,
     description=description,
     case_insensitive=True,
     activity=activity,
-    intents=intents
+    intents=intents,
 )
 
 
@@ -76,27 +74,41 @@ async def on_command_error(ctx, error):
     error = getattr(error, "original", error)
 
     if isinstance(error, (discord.Forbidden, commands.NoPrivateMessage)):
-        await ctx.send("I couldn't send this information to you via direct message. Are your DMs enabled?")
+        await ctx.send(
+            "I couldn't send this information to you via direct message. Are your DMs enabled?"
+        )
     elif isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send(f"You missed the argument `{error.param.name}` for this command!")
+        await ctx.send(
+            f"You missed the argument `{error.param.name}` for this command!"
+        )
     elif isinstance(error, commands.UserInputError):
-        await ctx.send(f"I can't understand this command message! Please check `!help {ctx.command}`")
+        await ctx.send(
+            f"I can't understand this command message! Please check `!help {ctx.command}`"
+        )
     elif isinstance(error, (commands.CommandNotFound, commands.CheckFailure)):
         return
     else:
-        error_traceback = traceback.format_exception(type(error), error, error.__traceback__)
-        print(f"Ignoring exception in command {ctx.command}:\n", *error_traceback, file=bot.error_file)
+        error_traceback = traceback.format_exception(
+            type(error), error, error.__traceback__
+        )
+        print(
+            f"Ignoring exception in command {ctx.command}:\n",
+            *error_traceback,
+            file=bot.error_file,
+        )
         bot.error_file.flush()
 
         await asyncio.gather(
             send_error_message(ctx, error, error_traceback),
-            ctx.send("The command you've entered could not be completed at this time.")
+            ctx.send("The command you've entered could not be completed at this time."),
         )
 
 
 async def send_error_message(ctx, error, tb):
     # Cleandoc was being really annoying for this, would be multiline str but was having issues.
-    error_message =  f"`{ctx.author}` running `{ctx.command}` caused `{error.__class__.__name__}`\n"
+    error_message = (
+        f"`{ctx.author}` running `{ctx.command}` caused `{error.__class__.__name__}`\n"
+    )
     error_message += f"Message Link: {ctx.message.jump_url}\n"
     error_message += f"```{''.join(tb)}```"
 
@@ -116,14 +128,16 @@ for folder in os.listdir("cogs"):
 
 async def startup():
     async with aiohttp.ClientSession() as session:
-        with open('error.log', 'w+') as error_file:
+        with open("error.log", "w+") as error_file:
             bot.session = session
             bot.error_file = error_file
             bot.loop = asyncio.get_event_loop()
 
-            await bot.start(os.getenv("TOKEN"))
+            print(f'{config.TOKEN=}')
+            await bot.start(config.TOKEN)
 
     print("Bot is going night-night.")
     await bot.close()
+
 
 asyncio.run(startup())
