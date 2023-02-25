31bb74393a7f9f00b63cbfc9ba770a9c793d527a
extreme4all <40169115+extreme4all@users.noreply.github.com>
2023-02-25 22:41:28 +0100

    cleanup

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -4,5 +4,3 @@ from src.api import v1
 
 router = APIRouter()
 router.include_router(v1.router, prefix="/v1")
-
-__all__ = ["router"]
--- src/api/v1/monitoring/__init__.py
+++ src/api/v1/monitoring/__init__.py
@@ -4,5 +4,3 @@ from src.api.v1.monitoring import health
 
 router = APIRouter()
 router.include_router(health.router, prefix="/health", tags=["Health"])
-
-__all__ = ["monitoring_router"]
